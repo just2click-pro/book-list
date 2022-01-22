478fcbbc591903ad08ce7b90bf710b6b10d7beed
Dror a <just2click@outlook.com>
2022-01-22 12:18:00 +0200

    Showcase array with map

--- src/index.js
+++ src/index.js
@@ -4,45 +4,32 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 // Setup
-const firstBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81V8R5aqDYL._AC_UL200_SR200,200_.jpg',
-  imgAlt: 'American Marxism',
-  author: 'Mark R Levin',
-  title: 'American Marxism',
-}
+const books = [
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81V8R5aqDYL._AC_UL200_SR200,200_.jpg',
+    imgAlt: 'American Marxism',
+    author: 'Mark R Levin',
+    title: 'American Marxism',
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91-EIJiYneL._AC_UL200_SR200,200_.jpg',
+    imgAlt: 'Atomic Habits',
+    author: 'James Clear',
+    title:
+      'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
+  },
+]
 
-const secondBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/91-EIJiYneL._AC_UL200_SR200,200_.jpg',
-  imgAlt: 'Atomic Habits',
-  author: 'James Clear',
-  title:
-    'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
-}
+const names = ['Mai', 'Racheli', 'Dror']
+const namesMap = names.map((name) => {
+  return <h1>{name}</h1>
+})
 
 const BookList = () => {
   return (
     <>
       <header></header>
-      <section className='booklist'>
-        <Book
-          img={firstBook.img}
-          imgAlt={firstBook.imgAlt}
-          author={firstBook.author}
-          title={firstBook.title}
-        >
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            aliquam facere! Dignissimos veniam necessitatibus repudiandae neque
-            iure cumque id blanditiis!
-          </p>
-        </Book>
-        <Book
-          img={secondBook.img}
-          imgAlt={secondBook.imgAlt}
-          author={secondBook.author}
-          title={secondBook.title}
-        />
-      </section>
+      <section className='booklist'>{namesMap}</section>
       <footer></footer>
     </>
   )
