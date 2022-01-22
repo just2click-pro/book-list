9845e7323ca9ff4b5c19e27ce1981bc4200f3392
Dror a <just2click@outlook.com>
2022-01-22 08:34:35 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const BookList = () => {
+  return (
+    <>
+      <header></header>
+      <section className="booklist">
+        <Book />
+        <Book />
+        <Book />
+        <Book />
+      </section>
+      <footer></footer>
+    </>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => {
+  return (
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/91-EIJiYneL._AC_UL200_SR200,200_.jpg"
+      alt="Atomic Habits"
+    ></img>
+  );
+};
+
+const Author = () => {
+  return <h4>James Clear</h4>;
+};
+
+const Title = () => {
+  return (
+    <h1>
+      Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones
+    </h1>
+  );
+};
+
+ReactDOM.render(<BookList />, document.querySelector("#root"));
